796395af98572b4680c48c8578abe1cbfbdb3c53
chickennight <112360329+chickennight@users.noreply.github.com>
2024-03-16 10:12:37 +0900

    [Bronze V] Title: A/B, Time: 116 ms, Memory: 13012 KB -BaekjoonHub

---
+++ 백준/Bronze/1008. A／B/A／B.java
@@ -0,0 +1,10 @@
+import java.util.*;
+ public class Main {
+    public static void main(String args[]){
+        Scanner imp = new Scanner(System.in);
+        double a, b;
+        a = imp.nextInt();
+        b = imp.nextInt();
+        System.out.println(a/b);
+    }
+}
