48b03094a836c2f8727d6aa5bcc5561e930030a6
chickennight <112360329+chickennight@users.noreply.github.com>
2024-03-16 10:11:36 +0900

    [Bronze V] Title: A+B, Time: 84 ms, Memory: 11504 KB -BaekjoonHub

---
+++ 백준/Bronze/1000. A＋B/A＋B.java
@@ -0,0 +1,12 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(in.readLine());
+        int a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
+        System.out.println(a + b);
+    }
+}
