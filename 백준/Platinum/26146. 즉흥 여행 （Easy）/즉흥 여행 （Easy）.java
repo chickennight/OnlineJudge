d6c5fdd429d8dcfef83e4e6734b4fdb05ef3a611
chickennight <112360329+chickennight@users.noreply.github.com>
2024-03-18 11:25:59 +0900

    [Platinum V] Title: 즉흥 여행 (Easy), Time: 1920 ms, Memory: 220844 KB -BaekjoonHub

---
+++ 백준/Platinum/26146. 즉흥 여행 （Easy）/즉흥 여행 （Easy）.java
@@ -0,0 +1,93 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static int V, E;
+    static List<List<Integer>> graph, RGraph, SCC;
+    static boolean[] visit;
+    static Stack<Integer> stack;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(in.readLine());
+        V = Integer.parseInt(st.nextToken());
+        E = Integer.parseInt(st.nextToken());
+
+        graph = new ArrayList<>();
+        RGraph = new ArrayList<>();
+        SCC = new ArrayList<>();
+        stack = new Stack<>();
+        visit = new boolean[V + 1];
+
+        for (int i = 0; i < V + 1; i++) {
+            graph.add(new ArrayList<>());
+            RGraph.add(new ArrayList<>());
+        }
+
+        for (int i = 0; i < E; i++) {
+            st = new StringTokenizer(in.readLine());
+            int start = Integer.parseInt(st.nextToken());
+            int end = Integer.parseInt(st.nextToken());
+
+            graph.get(start).add(end);
+            RGraph.get(end).add(start);
+        }
+
+        for (int i = 1; i <= V; i++) {
+            if (!visit[i]) {
+                DFS(i);
+            }
+        }
+
+        Arrays.fill(visit, false);
+
+        int sccNum = 0;
+        SCC.add(new ArrayList<>());
+        while (!stack.isEmpty()) {
+            int node = stack.pop();
+            if (!visit[node]) {
+                RDFS(node, sccNum);
+                sccNum++;
+                SCC.add(new ArrayList<>());
+            }
+        }
+        SCC.remove(SCC.size() - 1);
+
+        for (List<Integer> s : SCC) {
+            Collections.sort(s);
+        }
+
+        SCC.sort(Comparator.comparingInt(o -> o.get(0)));
+        String ans = "Yes";
+        for (List<Integer> s : SCC) {
+            if (s.size() != V) {
+                ans = "No";
+                break;
+            }
+        }
+        System.out.println(ans);
+    }
+
+    private static void RDFS(int node, int sccNum) {
+        visit[node] = true;
+
+        SCC.get(sccNum).add(node);
+
+        for (int i : RGraph.get(node)) {
+            if (!visit[i]) {
+                RDFS(i, sccNum);
+            }
+        }
+    }
+
+    private static void DFS(int node) {
+        visit[node] = true;
+
+        for (int i : graph.get(node)) {
+            if (!visit[i]) {
+                DFS(i);
+            }
+        }
+        stack.push(node);
+    }
+}
