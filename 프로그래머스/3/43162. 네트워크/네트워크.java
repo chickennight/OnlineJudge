e54fc93ac6ac064cbad697c252985dc2f5e7c6bf
chickennight <112360329+chickennight@users.noreply.github.com>
2024-03-22 18:44:31 +0900

    [level 3] Title: 네트워크, Time: 0.62 ms, Memory: 72.6 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.java
+++ 프로그래머스/3/43162. 네트워크/네트워크.java
@@ -5,28 +5,29 @@ class Solution {
     public int solution(int n, int[][] computers) {
         int answer = 0;
         List<Integer>[] list = new ArrayList[n+1];
-        for(int i=1;i<=n;++i)
+        for(int i=1; i<=n; ++i)
             list[i] = new ArrayList<>();
         
-        for(int i=0;i<n;++i)
-            for(int j=0;j<n;++j)
-                if(i!=j&&computers[i][j]==1){
+        for(int i=0; i<n; ++i)
+            for(int j=0; j<n; ++j)
+                if(i!=j && computers[i][j]==1){
                     list[i+1].add(j+1);
                     list[j+1].add(i+1);
                 }
         
-        boolean[] visit = new boolean[n+1];
+        boolean[] visited = new boolean[n+1];
         Queue<Integer> q = new LinkedList<>();
-        for(int i=1;i<=n;++i){
-            if(!visit[i]){
-                visit[i] = true;
+        int now;
+        for(int i=1; i<=n; ++i){
+            if(!visited[i]){
+                visited[i] = true;
                 q.add(i);
                 answer++;
                 while(!q.isEmpty()){
-                    int now = q.poll();
-                    for(int next:list[now]){
-                        if(!visit[next]){
-                            visit[next] = true;
+                    now = q.poll();
+                    for(int next: list[now]){
+                        if(!visited[next]){
+                            visited[next] = true;
                             q.add(next);
                         }
                     }
