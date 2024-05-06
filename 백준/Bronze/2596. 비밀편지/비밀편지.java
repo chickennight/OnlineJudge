d2c190475a27e1bf1d751b3c3a08ce101c149b97
chickennight <112360329+chickennight@users.noreply.github.com>
2024-05-06 11:47:59 +0900

    [Bronze I] Title: 비밀편지, Time: 132 ms, Memory: 14416 KB -BaekjoonHub

---
+++ 백준/Bronze/2596. 비밀편지/비밀편지.java
@@ -0,0 +1,60 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        int[][] arr = new int[8][7];
+        int n = Integer.parseInt(in.readLine());
+        String s = in.readLine();
+        StringBuilder ans = new StringBuilder();
+        char c;
+        int bit;
+        boolean chk;
+
+        arr[0][0] = 0B000000;
+        arr[1][0] = 0B001111;
+        arr[2][0] = 0B010011;
+        arr[3][0] = 0B011100;
+        arr[4][0] = 0B100110;
+        arr[5][0] = 0B101001;
+        arr[6][0] = 0B110101;
+        arr[7][0] = 0B111010;
+
+        for (int i = 0; i < 8; ++i) {
+            for (int j = 1; j <= 6; ++j) {
+                arr[i][j] = (arr[i][0] ^ (1 << j - 1));
+            }
+        }
+
+        for (int i = 0; i < n; ++i) {
+            bit = 0;
+            for (int j = 0; j < 6; ++j) {
+                if (s.charAt(i * 6 + j) - '0' == 1) {
+                    bit = bit | (1 << 5 - j);
+                }
+            }
+            chk = false;
+            for (int j = 0; j < 8; ++j) {
+                if (chk) {
+                    break;
+                }
+                for (int k = 0; k <= 6; ++k) {
+                    if (arr[j][k] == bit) {
+                        c = (char) ('A' + j);
+                        ans.append(c);
+                        chk = true;
+                        break;
+                    }
+                }
+            }
+            if (!chk) {
+                System.out.printf("%d%n", i + 1);
+                return;
+            }
+        }
+        System.out.printf("%s%n", ans);
+    }
+}
+
