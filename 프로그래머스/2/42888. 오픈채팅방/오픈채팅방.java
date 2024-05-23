67a341969b34897b715b10699ee6edf22042042b
chickennight <112360329+chickennight@users.noreply.github.com>
2024-05-23 20:29:26 +0900

    [level 2] Title: 오픈채팅방, Time: 131.67 ms, Memory: 142 MB -BaekjoonHub

---
+++ 프로그래머스/2/42888. 오픈채팅방/오픈채팅방.java
@@ -0,0 +1,34 @@
+import java.util.*;
+class Solution {
+    private List<String> answer;
+    private Map<String, String> map;
+    public String[] solution(String[] record) {
+        map = new HashMap<>();
+        answer = new ArrayList<>();
+        for(String order: record)
+            inputUser(order);
+        
+        for(String order: record)
+            generateMessage(order);
+        
+        return answer.toArray(new String[answer.size()]);
+    }
+    
+    private void inputUser(String order){
+        String[] orders = order.split(" ");
+        if(orders[0].equals("Enter")){
+            map.put(orders[1],orders[2]);
+        } else if(orders[0].equals("Change")){
+            map.put(orders[1],orders[2]);
+        }
+    }
+    
+    private void generateMessage(String order){
+        String[] orders = order.split(" ");
+        if(orders[0].equals("Enter")){
+            answer.add(map.get(orders[1])+"님이 들어왔습니다.");
+        }else if(orders[0].equals("Leave")){
+            answer.add(map.get(orders[1])+"님이 나갔습니다.");
+        }
+    }
+}
