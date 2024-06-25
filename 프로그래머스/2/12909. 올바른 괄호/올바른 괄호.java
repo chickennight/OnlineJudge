140077187ccf5706c358f9019872fc01a0bae80a
chickennight <112360329+chickennight@users.noreply.github.com>
2024-06-25 14:32:45 +0900

    [level 2] Title: 올바른 괄호, Time: 6.60 ms, Memory: 62.4 MB -BaekjoonHub

---
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
@@ -0,0 +1,15 @@
+class Solution {
+    boolean solution(String s) {
+        int cnt = 0;
+        for(int i=0; i<s.length(); ++i){
+            char c = s.charAt(i);
+            if(c == '(') cnt++;
+            else{
+                if(cnt > 0) cnt--;
+                else return false;
+            }
+        }
+
+        return cnt == 0;
+    }
+}
