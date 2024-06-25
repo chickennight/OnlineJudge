a67d87e4316405181854a2d21c27a5bead4cc078
chickennight <112360329+chickennight@users.noreply.github.com>
2024-06-25 17:56:57 +0900

    [level 2] Title: 피보나치 수, Time: 2.83 ms, Memory: 78.1 MB -BaekjoonHub

---
+++ 프로그래머스/2/12945. 피보나치 수/피보나치 수.java
@@ -0,0 +1,11 @@
+class Solution {
+    final int MOD = 1234567;
+    public int solution(int n) {
+        int[] dp = new int[n+1];
+        dp[1] = 1;
+        for(int i=2; i<=n; ++i){
+            dp[i] = (dp[i-1] + dp[i-2])%MOD;
+        }
+        return dp[n];
+    }
+}
