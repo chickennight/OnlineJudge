9ebec4999de32d88b1ddbf095ca684b55a4e9dff
chickennight <112360329+chickennight@users.noreply.github.com>
2024-07-01 14:10:47 +0900

    [level 2] Title: 연속 부분 수열 합의 개수, Time: 1564.13 ms, Memory: 120 MB -BaekjoonHub

---
+++ 프로그래머스/2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.java
@@ -0,0 +1,21 @@
+import java.util.*;
+class Solution {
+    public int solution(int[] elements) {
+        
+        int size = elements.length;
+        
+        Set<Integer> set = new HashSet<>();
+
+        for(int i=1; i<=size; ++i){
+            for(int j=0; j<size; ++j){
+                int sum = 0;
+                for(int k=j; k<j+i; ++k){
+                    sum += elements[k%size];
+                }
+                set.add(sum);
+            }
+        }
+
+        return set.size();
+    }
+}
