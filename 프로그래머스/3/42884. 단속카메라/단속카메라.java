87ff2452801c98ca6757e0e34b4fb9e8e73ef09e
chickennight <112360329+chickennight@users.noreply.github.com>
2024-07-18 20:44:37 +0900

    [level 3] Title: 단속카메라, Time: 9.32 ms, Memory: 54 MB -BaekjoonHub

--- 프로그래머스/3/42884. 단속카메라/단속카메라.java
+++ 프로그래머스/3/42884. 단속카메라/단속카메라.java
@@ -1,19 +1,19 @@
+// 단속 카메라
 import java.util.*;
-
 class Solution {
     public int solution(int[][] routes) {
         int answer = 1;
 
-        Arrays.sort(routes, (o1,o2)->{
-            return o1[1]-o2[1];
+        Arrays.sort(routes, (o1, o2)->{
+            return o1[1] - o2[1];
         });
 
         int max = routes[0][1];
 
-        for(int i=1; i<routes.length; ++i){
-            if(routes[i][0]<=max && routes[i][1]>=max)
+        for(int i = 1; i < routes.length ; ++i){
+            if(routes[i][0] <= max && routes[i][1] >= max){
                 continue;
-            else{
+            }else{
                 max = routes[i][1];
                 answer++;
             }
