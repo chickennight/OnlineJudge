9c25e3dd1ed0b63d2a064d03fa88a41bfe50dfa0
chickennight <112360329+chickennight@users.noreply.github.com>
2024-08-01 21:10:31 +0900

    [level 2] Title: 숫자 변환하기, Time: 100.55 ms, Memory: 104 MB -BaekjoonHub

--- 프로그래머스/2/154538. 숫자 변환하기/숫자 변환하기.java
+++ 프로그래머스/2/154538. 숫자 변환하기/숫자 변환하기.java
@@ -2,8 +2,11 @@ import java.util.*;
 class Solution {
     public int solution(int x, int y, int n) {
         if(x == y) return 0;
+        
         boolean[] visited = new boolean[y+1];
+        
         Queue<Integer> q = new LinkedList<>();
+        
         q.add(x);
         visited[x] = true;
         int cnt = 1, size, now, next;
@@ -12,21 +15,29 @@ class Solution {
             while(size-- > 0){
                 now = q.poll();
                 next = now + n;
+                
                 if(next > y) continue;
+                
                 if(!visited[next]){
                     if(next == y) return cnt;
                     visited[next] = true;
                     q.add(next);
                 }
+                
                 next = now * 2;
+                
                 if(next > y) continue;
+                
                 if(!visited[next]){
                     if(next == y) return cnt;
                     visited[next] = true;
                     q.add(next);
                 }
+                
                 next = now * 3;
+                
                 if(next > y) continue;
+                
                 if(!visited[next]){
                     if(next == y) return cnt;
                     visited[next] = true;
