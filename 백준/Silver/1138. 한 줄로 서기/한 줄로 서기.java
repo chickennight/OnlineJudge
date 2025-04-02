bc22c26abb8b2cc040382cd309e1272bdc12be8f
chickennight <112360329+chickennight@users.noreply.github.com>
2025-04-02 20:14:20 +0900

    [Silver II] Title: 한 줄로 서기, Time: 100 ms, Memory: 14156 KB -BaekjoonHub

--- 백준/Silver/1138. 한 줄로 서기/한 줄로 서기.java
+++ 백준/Silver/1138. 한 줄로 서기/한 줄로 서기.java
@@ -1,24 +1,26 @@
+// 1138 한 줄로 서기
+
 import java.io.*;
 import java.util.*;
 
 public class Main {
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws Exception {
         BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+
         StringBuilder sb = new StringBuilder();
 
-        int n = Integer.parseInt(in.readLine().trim());
-        int[] tall = new int[n + 1];
+        int n = Integer.parseInt(in.readLine());
+        int[] length = new int[n + 1];
         List<Integer> ans = new ArrayList<>();
 
         StringTokenizer st = new StringTokenizer(in.readLine());
         for (int i = 1; i <= n; ++i)
-            tall[i] = Integer.parseInt(st.nextToken());
+            length[i] = Integer.parseInt(st.nextToken());
 
 
         for (int i = n; i >= 1; --i)
-            ans.add(tall[i], i);
-
+            ans.add(length[i], i);
 
         for (int k : ans)
             sb.append(k).append(" ");
@@ -26,3 +28,4 @@ public class Main {
         System.out.println(sb);
     }
 }
+
