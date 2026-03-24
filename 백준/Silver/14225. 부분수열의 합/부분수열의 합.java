236ebd5a56ceab3de2cfbeba4998b0257df499a8
chickennight <112360329+chickennight@users.noreply.github.com>
2026-03-24 23:07:52 +0900

    [Silver I] Title: 부분수열의 합, Time: 116 ms, Memory: 16236 KB -BaekjoonHub

--- 백준/Silver/14225. 부분수열의 합/부분수열의 합.java
+++ 백준/Silver/14225. 부분수열의 합/부분수열의 합.java
@@ -1,48 +1,44 @@
 // 14225 부분수열의 합
 
 import java.io.*;
-import java.text.BreakIterator;
 import java.util.*;
 
 public class Main {
+
+    private static int[] arr;
+    private static final boolean[] num = new boolean[2000001];
 
     public static void main(String[] args) throws Exception {
         BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
 
         int N = Integer.parseInt(in.readLine());
+        arr = new int[N];
+
         StringTokenizer st = new StringTokenizer(in.readLine());
-        int[] arr = new int[N];
         for (int i = 0; i < N; ++i)
             arr[i] = Integer.parseInt(st.nextToken());
 
-        Set<Integer> set = new HashSet<>();
-        boolean[] use = new boolean[N];
-        recursive(set, arr, use, 0);
-
-        for (int i = 1; i < 100000 * 20; ++i) {
-            if (!set.contains(i)) {
-                System.out.println(i);
+        dfs(0, 0);
+        int result = 0;
+        for (int i = 1; i < num.length; ++i) {
+            if (!num[i]) {
+                result = i;
                 break;
             }
         }
+
+        System.out.print(result);
     }
 
-    private static void recursive(Set<Integer> set, int[] arr, boolean[] use, int idx) {
+    private static void dfs(int idx, int sum) {
         if (idx == arr.length) {
-            int num = 0;
-            for (int i = 0; i < arr.length; ++i) {
-                if (use[i]) {
-                    num += arr[i];
-                }
-            }
-            set.add(num);
+            num[sum] = true;
             return;
         }
 
-        use[idx] = true;
-        recursive(set, arr, use, idx + 1);
-        use[idx] = false;
-        recursive(set, arr, use, idx + 1);
+        dfs(idx + 1, sum + arr[idx]);
+
+        dfs(idx + 1, sum);
     }
 
-}
+} 
