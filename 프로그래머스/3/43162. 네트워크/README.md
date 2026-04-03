19d2c25740e846ede81f95abad0ec27eb5d705ef
chickennight <112360329+chickennight@users.noreply.github.com>
2026-04-04 00:12:51 +0900

    [level 3] Title: 네트워크, Time: 0.47 ms, Memory: 89.6 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/README.md
+++ 프로그래머스/3/43162. 네트워크/README.md
@@ -1,10 +1,10 @@
 # [level 3] 네트워크 - 43162 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43162) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43162?language=java) 
 
 ### 성능 요약
 
-메모리: 65.6 MB, 시간: 0.19 ms
+메모리: 89.6 MB, 시간: 0.47 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 05월 18일 09:36:03
+2026년 04월 04일 00:12:49
 
 ### 문제 설명
 
--- 프로그래머스/3/43162. 네트워크/네트워크.java
+++ 프로그래머스/3/43162. 네트워크/네트워크.java
@@ -1,8 +1,22 @@
 import java.util.*;
 
 class Solution {
+    private boolean[] visited;
+    private int[][] computers;
+    public int solution(int n, int[][] computerArr) {
+        visited = new boolean[n];
+        computers = computerArr;
+        int answer = 0;
+        for(int i=0; i<n; ++i){
+            if(!visited[i]){
+                bfs(i);
+                answer++;
+            }
+        }
+        return answer;
+    }
     
-    private void bfs(int num, boolean[] visited, int[][] computers){
+    private void bfs(int num){
         Queue<Integer> q = new LinkedList<>();
         q.add(num);
         visited[num]= true;
@@ -14,62 +28,6 @@ class Solution {
                 visited[i] = true;
             }
         }
-    }
-    
-    public int solution(int n, int[][] computers) {
-        
-        boolean[] visited = new boolean[n];
-        int answer = 0;
-        for(int i=0; i<n; ++i){
-            if(!visited[i]){
-                bfs(i, visited, computers);
-                answer++;
-            }
-        }
-        return answer;
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-//         int answer = 0;
-//         List<Integer>[] list = new ArrayList[n+1];
-//         for(int i=1; i<=n; ++i)
-//             list[i] = new ArrayList<>();
-        
-//         for(int i=0; i<n; ++i)
-//             for(int j=0; j<n; ++j)
-//                 if(i!=j && computers[i][j]==1){
-//                     list[i+1].add(j+1);
-//                     list[j+1].add(i+1);
-//                 }
-        
-//         boolean[] visited = new boolean[n+1];
-//         Queue<Integer> q = new LinkedList<>();
-//         int now;
-//         for(int i=1; i<=n; ++i){
-//             if(!visited[i]){
-//                 visited[i] = true;
-//                 q.add(i);
-//                 answer++;
-//                 while(!q.isEmpty()){
-//                     now = q.poll();
-//                     for(int next: list[now]){
-//                         if(!visited[next]){
-//                             visited[next] = true;
-//                             q.add(next);
-//                         }
-//                     }
-//                 }
-//             }
-//         }
-        
-//         return answer;
     }
     
 }
