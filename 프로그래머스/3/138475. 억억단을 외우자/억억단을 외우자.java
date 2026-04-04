da6eff3fb03de147818d65552c3e94ea01117f35
chickennight <112360329+chickennight@users.noreply.github.com>
2026-04-04 23:12:48 +0900

    [level 3] Title: 억억단을 외우자, Time: 325.59 ms, Memory: 135 MB -BaekjoonHub

---
+++ 프로그래머스/3/138475. 억억단을 외우자/억억단을 외우자.java
@@ -0,0 +1,32 @@
+class Solution {
+    public int[] solution(int e, int[] starts) {
+        int[] answer = new int[starts.length];
+        int[] dic = new int[e+1];
+        int[] cnt = new int[e+1];
+        dic[1] = 1;
+        cnt[1] = 1;
+        for(int i=2; i<=e; ++i){
+            int n = e/i;
+            int j = i;
+            for(int k=1; k<=n; ++k){
+                dic[j] += 1;
+                j += i;
+            }   
+        }
+        
+        int v = 0;
+        for(int i=e; i>=1; --i){
+            if(dic[i] >= v){
+                v = dic[i];
+                cnt[i] = i;
+            }else{
+                cnt[i] = cnt[i+1];
+            }
+        }
+        
+        for(int i=0; i<starts.length; ++i){
+            answer[i] = cnt[starts[i]];
+        }
+        return answer;
+    }
+}
