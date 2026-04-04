93d30e1db52104ad8bad2426e3071e923149c449
chickennight <112360329+chickennight@users.noreply.github.com>
2026-04-04 18:37:02 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 7.00 ms, Memory: 55.3 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/README.md
+++ 프로그래머스/2/1844. 게임 맵 최단거리/README.md
@@ -1,10 +1,10 @@
 # [level 2] 게임 맵 최단거리 - 1844 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/1844) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/1844?language=java) 
 
 ### 성능 요약
 
-메모리: 53.9 MB, 시간: 5.86 ms
+메모리: 55.3 MB, 시간: 7.00 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 03월 28일 20:01:39
+2026년 04월 04일 18:36:58
 
 ### 문제 설명
 
--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
@@ -1,43 +1,45 @@
 import java.util.*;
 
 class Solution {
-    private int[] dx = {1,-1,0,0};
-    private int[] dy = {0,0,1,-1};
-    private int n,m;
+    private int[] dx = {1, -1, 0, 0};
+    private int[] dy = {0, 0, 1, -1};
     
     public int solution(int[][] maps) {
-        n = maps.length;
-        m = maps[0].length;
-        int[][] record = new int[n][m];
-        for(int i=0; i<n; ++i)
-            Arrays.fill(record[i],-1);
+        int w = maps[0].length;
+        int h = maps.length;
+        int[][] arr = new int[h][w];
+        for(int i=0; i<h; ++i)
+            Arrays.fill(arr[i],-1);
         
-        Queue<int[]> q = new LinkedList<>();
-        
-        record[0][0] = 1;
+        Queue<int[]> q = new ArrayDeque<>();
+        arr[0][0] = 0;
         q.add(new int[]{0,0});
-        
-        int[] now;
-        int x,y,nx,ny;
+        int size;
+        int cnt = 2;
         while(!q.isEmpty()){
-            now = q.poll();
-            x = now[0];
-            y = now[1];
-            
-            for(int d=0; d<4; ++d){
-                nx = x + dx[d];
-                ny = y + dy[d];
-                if(inRange(nx,ny) && maps[nx][ny]==1 && record[nx][ny] == -1){
-                    record[nx][ny] = record[x][y] + 1;
+            size = q.size();
+            while(size-->0){
+                int[] now = q.poll();
+                for(int d=0; d<4; ++d){
+                    int nx = now[0] + dx[d];
+                    int ny = now[1] + dy[d];
+                    
+                    if(nx>=h||nx<0 || ny>=w||ny<0) continue;
+                    
+                    if(arr[nx][ny]>=0 || maps[nx][ny]==0) continue;
+                    
+                    arr[nx][ny] = cnt;
+                    
+                    if(nx==h-1 && ny==w-1) return cnt;
+                    
                     q.add(new int[]{nx,ny});
                 }
             }
+            cnt++;
         }
         
-        return record[n-1][m-1];
+        
+        return -1;
     }
-    
-    private boolean inRange(int x, int y){
-        return x>=0 && x<n && y>=0 && y<m;
-    }
+
 }
